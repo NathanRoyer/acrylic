3f9d66bd053487904b02daaf00ce7462e9792c22
Nathan Royer <nathan.royer.pro@gmail.com>
2022-05-23 00:34:34 +0200

    feat: the git repository is now a workspace

---
+++ acrylic-wayland/src/lib.rs
@@ -0,0 +1,325 @@
+use std::fs::File;
+use std::fs::read;
+use std::os::unix::io::AsRawFd;
+use std::collections::HashMap;
+pub use std::concat;
+
+use wayland_client::GlobalManager;
+use wayland_client::EventQueue;
+use wayland_client::Interface;
+use wayland_client::Attached;
+use wayland_client::Display;
+use wayland_client::Main;
+use wayland_client::protocol::wl_compositor::WlCompositor;
+use wayland_client::protocol::wl_surface::WlSurface;
+use wayland_client::protocol::wl_shm::WlShm;
+use wayland_client::protocol::wl_shm::Format;
+use wayland_client::protocol::wl_display::WlDisplay;
+use wayland_client::protocol::wl_shm_pool::WlShmPool;
+use wayland_client::protocol::wl_buffer::WlBuffer;
+
+use wayland_protocols::xdg_shell::client::xdg_wm_base::XdgWmBase;
+use wayland_protocols::xdg_shell::client::xdg_toplevel::XdgToplevel;
+use wayland_protocols::xdg_shell::client::xdg_wm_base::Event as XdgWmBaseEvent;
+use wayland_protocols::xdg_shell::client::xdg_surface::Event as XdgSurfaceEvent;
+use wayland_protocols::xdg_shell::client::xdg_surface::XdgSurface;
+use wayland_protocols::xdg_shell::client::xdg_toplevel::Event as XdgToplevelEvent;
+
+use acrylic::bitmap::RGBA;
+use acrylic::app::Application;
+use acrylic::app::sub_spot;
+use acrylic::app::Style;
+use acrylic::node::NodePath;
+use acrylic::Point;
+use acrylic::Size;
+use acrylic::Spot;
+
+use tempfile::tempfile;
+
+use memmap::MmapOptions;
+use memmap::MmapMut;
+
+pub struct FrameBuffer {
+	pool_size: usize,
+	file: File,
+	pool: Main<WlShmPool>,
+	buffer: Main<WlBuffer>,
+	data: MmapMut,
+}
+
+impl FrameBuffer {
+	pub fn new(shm: &WlShm, size: Size) -> Self {
+		let len = RGBA * size.w * size.h;
+		let file = tempfile().unwrap();
+		file.set_len(len as u64).unwrap();
+		let pool = shm.create_pool(file.as_raw_fd(), len as i32);
+		let fmt = Format::Abgr8888;
+		let (w, h) = (size.w as i32, size.h as i32);
+		let buffer = pool.create_buffer(0, w, h, w * (RGBA as i32), fmt);
+		let data = unsafe {
+			MmapOptions::new()
+				.len(len)
+				.map_mut(&file).unwrap()
+		};
+		Self {
+			pool_size: len,
+			file,
+			pool,
+			buffer,
+			data,
+		}
+	}
+
+	pub fn resize(&mut self, size: Size) {
+		let len = RGBA * size.w * size.h;
+		if len > self.pool_size {
+			self.file.set_len(len as u64).unwrap();
+			self.pool.resize(len as i32);
+			self.pool_size = len;
+		}
+		self.buffer.destroy();
+		let fmt = Format::Abgr8888;
+		let (w, h) = (size.w as i32, size.h as i32);
+		self.buffer = self.pool.create_buffer(0, w, h, w * (RGBA as i32), fmt);
+		self.data = unsafe {
+			MmapOptions::new()
+				.len(len)
+				.map_mut(&self.file).unwrap()
+		};
+		self.data.fill(0);
+	}
+}
+
+#[allow(dead_code)]
+struct WaylandApp {
+	pub display: Attached<WlDisplay>,
+	pub global_manager: GlobalManager,
+	pub compositor: Main<WlCompositor>,
+	pub xdg_wm_base: Main<XdgWmBase>,
+	pub shm: Main<WlShm>,
+	pub frame_buffer: FrameBuffer,
+	pub surface: Main<WlSurface>,
+	pub xdg_surface: Main<XdgSurface>,
+	pub xdg_toplevel: Main<XdgToplevel>,
+	pub acrylic_app: Application,
+	pub closed: bool,
+	pub configured: bool,
+	pub can_run: bool,
+	pub size: Size,
+}
+
+impl WaylandApp {
+	pub fn new(acrylic_app: Application) -> (Self, EventQueue) {
+		let display = Display::connect_to_env().unwrap();
+		let mut event_queue = display.create_event_queue();
+		let display = display.attach(event_queue.token());
+		let gm = GlobalManager::new(&display);
+		event_queue.dispatch(&mut (), |_,_,_| unreachable!()).expect("Event dispatching Error!");
+
+		let compositor = gm.instantiate_range::<WlCompositor>(0, WlCompositor::VERSION).unwrap();
+		let xdg_wm_base = gm.instantiate_range::<XdgWmBase>(0, XdgWmBase::VERSION).unwrap();
+		let shm = gm.instantiate_range::<WlShm>(0, WlShm::VERSION).unwrap();
+
+		xdg_wm_base.quick_assign(|iface, event, _data| {
+			if let XdgWmBaseEvent::Ping { serial } = event {
+				iface.pong(serial);
+			}
+		});
+
+		shm.quick_assign(|_iface, event, _data| {
+			if let wayland_client::protocol::wl_shm::Event::Format { format } = event {
+				println!("{:?}, {}", format, format as u32);
+			}
+		});
+
+		let surface = compositor.create_surface();
+		let xdg_surface = xdg_wm_base.get_xdg_surface(&surface);
+		let xdg_toplevel = xdg_surface.get_toplevel();
+		xdg_toplevel.quick_assign(|_iface, event, mut data| {
+			let app = data.get::<WaylandApp>().unwrap();
+			if let XdgToplevelEvent::Configure { width, height, .. } = event {
+				if app.configured {
+					let (w, h) = match (width, height) {
+						(0, 0) => (500, 500),
+						_ => (width as usize, height as usize),
+					};
+					app.size = Size::new(w, h);
+					let spot = (Point::zero(), app.size);
+					app.acrylic_app.set_spot(spot);
+					app.frame_buffer.resize(app.size);
+					unsafe {
+						FRAME_SPOT = Some(spot);
+						BG_PIXELS.as_mut().unwrap().resize(w * h * RGBA, 0);
+					};
+					app.surface.attach(Some(&app.frame_buffer.buffer), 0, 0);
+					app.can_run = true;
+					app.request_frame();
+					app.surface.commit();
+				}
+			} else if let XdgToplevelEvent::Close = event {
+				app.closed = true;
+			}
+		});
+
+		xdg_surface.quick_assign(|xdg_surface, event, mut data| {
+			let app = data.get::<WaylandApp>().unwrap();
+			if let XdgSurfaceEvent::Configure { serial } = event {
+				app.configured = true;
+				xdg_surface.ack_configure(serial);
+				app.surface.attach(Some(&app.frame_buffer.buffer), 0, 0);
+				app.surface.commit();
+			}
+		});
+
+		let size = Size::new(500, 500);
+		let frame_buffer = FrameBuffer::new(&shm, size);
+		let app = WaylandApp {
+			acrylic_app,
+			display,
+			global_manager: gm,
+			compositor,
+			xdg_wm_base,
+			shm,
+			frame_buffer,
+			surface,
+			xdg_surface,
+			xdg_toplevel,
+			size,
+			closed: false,
+			configured: false,
+			can_run: false,
+		};
+		app.surface.commit();
+		(app, event_queue)
+	}
+
+	pub fn frame(&mut self, assets: &str) {
+		self.frame_buffer.data.copy_from_slice(unsafe {
+			&mut BG_PIXELS.as_mut().unwrap()
+		});
+		self.acrylic_app.render();
+		while let Some(request) = self.acrylic_app.data_requests.pop() {
+			println!("loading {}", request.name);
+			let data = read(&format!("{}{}", assets, request.name)).unwrap();
+			let node = self.acrylic_app.get_node(&request.node).unwrap();
+			let mut node = node.lock().unwrap();
+			node.loaded(&mut self.acrylic_app, &request.node, &request.name, 0, &data);
+		}
+		let blits = unsafe { BLITS_PIXELS.as_ref().unwrap() };
+		let mut paths = blits.keys().collect::<Vec<&NodePath>>();
+		paths.sort_by(|a, b| b.len().partial_cmp(&a.len()).unwrap());
+		for path in paths {
+			if let Some(((pos, size), pixels)) = blits.get(path) {
+				if size.w != 0 && size.h != 0 && size.w < self.size.w {
+					let (x, y) = (pos.x as usize, pos.y as usize);
+					let start = RGBA * (x + y * self.size.w);
+					let pitch = RGBA * (self.size.w - size.w);
+					let stop = start + RGBA * (size.w + size.h * self.size.w);
+					let px_width = RGBA * size.w;
+					let mut dst = self.frame_buffer.data.get_mut(start..stop);
+					let mut src = pixels.chunks(px_width);
+					while let (Some(dst_tmp), Some(src)) = (dst, src.next()) {
+						let (line_dst, next_dst) = dst_tmp.split_at_mut(px_width);
+						let mut i = px_width as isize - 1;
+						let (mut a, mut b) = (0, 0);
+						while i >= 0 {
+							let j = i as usize;
+							let (dst, src) = (&mut line_dst[j], &(src[j] as u32));
+							if (j & 0b11) == 3 {
+								a = *src as u32;
+								b = 255 - a;
+							}
+							*dst = ((*src * a + (*dst as u32) * b) / 255) as u8;
+							i -= 1;
+						}
+						dst = next_dst.get_mut(pitch..);
+					}
+				}
+			}
+		}
+		self.surface.damage_buffer(0, 0, self.size.w as i32, self.size.h as i32);
+	}
+
+	pub fn request_frame(&mut self) {
+		// println!("requesting frame");
+		self.surface.frame().quick_assign(|_iface, _event, mut data| {
+			let app = data.get::<WaylandApp>().unwrap();
+			app.request_frame();
+		});
+		self.surface.commit();
+	}
+}
+
+pub static mut BLITS_PIXELS: Option<HashMap<NodePath, (Spot, Vec<u8>)>> = None;
+pub static mut BG_PIXELS: Option<Vec<u8>> = None;
+pub static mut FRAME_SPOT: Option<Spot> = None;
+
+pub fn blit<'a>(spot: &'a Spot, path: Option<&'a NodePath>) -> Option<(&'a mut [u8], usize, bool)> {
+	if let Some(path) = path {
+		let (_, size) = *spot;
+		let total_pixels = size.w * size.h * RGBA;
+		let slice = unsafe {
+			let blits = BLITS_PIXELS.as_mut().unwrap();
+			if let None = blits.get_mut(path) {
+				let pixels = vec![0; total_pixels];
+				let spot = (Point::zero(), Size::zero());
+				blits.insert(path.clone(), (spot, pixels));
+			}
+			let (saved_spot, vec) = blits.get_mut(path).unwrap();
+			*saved_spot = *spot;
+			vec.resize(total_pixels, 0);
+			&mut *vec
+		};
+		Some((slice, 0, true))
+	} else {
+		let f_spot = unsafe { FRAME_SPOT.unwrap() };
+		let background = unsafe { BG_PIXELS.as_mut().unwrap() };
+		let background = background.as_mut_slice();
+		let (slice, pitch) = sub_spot(background, 0, [&f_spot, spot])?;
+		Some((slice, pitch, true))
+	}
+}
+
+pub fn run(mut app: Application, assets: &str) {
+	app.set_styles(vec![
+		Style {
+			background: [50, 50, 50, 255],
+			foreground: [255; RGBA],
+			border:     [0; RGBA],
+		},
+		Style {
+			background: [100, 100, 100, 255],
+			foreground: [100, 200, 100, 255],
+			border:     [0; RGBA],
+		},
+		Style {
+			background: [50, 50, 250, 255],
+			foreground: [255; RGBA],
+			border:     [0; RGBA],
+		}
+	]);
+	unsafe { BLITS_PIXELS = Some(HashMap::new()) };
+	unsafe { BG_PIXELS = Some(Vec::new()) };
+	let (mut app, mut event_queue) = WaylandApp::new(app);
+	while !app.closed {
+		if app.can_run {
+			app.frame(assets);
+		}
+		event_queue
+			.dispatch(&mut app, |a, _, _| println!("ignored {:?}", a))
+			.expect("Event dispatching Error!");
+	}
+}
+
+#[macro_export]
+macro_rules! app {
+	($path: literal, $init: block) => {
+		fn main() {
+			platform::run($init, $path);
+		}
+	}
+}
+
+pub fn log(message: &str) {
+	println!("{}", message);
+}
---
+++ acrylic-web/src/lib.rs
@@ -0,0 +1,202 @@
+use acrylic::app::Application;
+use acrylic::app::Style;
+use acrylic::app::sub_spot;
+use acrylic::node::NodePath;
+use acrylic::bitmap::RGBA;
+use acrylic::Spot;
+use acrylic::Size;
+use acrylic::Point;
+
+use std::collections::HashMap;
+use std::mem::swap;
+
+extern "C" {
+	fn raw_log(s: *const u8, l: usize);
+	fn raw_set_request_url(s: *const u8, l: usize);
+	fn raw_set_request_url_prefix(s: *const u8, l: usize);
+	fn raw_update_blit(x: isize, y: isize, w: usize, h: usize, px: *const u8, d: usize, p: *const u8, l: usize);
+	fn raw_set_blit_dirty(p: *const u8, l: usize);
+	fn raw_is_request_pending() -> usize;
+}
+
+pub fn log(s: &str) {
+	unsafe { raw_log(s.as_ptr(), s.len()) };
+}
+
+pub fn set_blit_dirty(m: &str) {
+	unsafe {
+		raw_set_blit_dirty(m.as_ptr(), m.len())
+	};
+}
+
+pub fn update_blit(p: Point, s: Size, px: &[u8], d: usize, m: &str) {
+	unsafe {
+		raw_update_blit(p.x, p.y, s.w, s.h, px.as_ptr(), d, m.as_ptr(), m.len())
+	};
+}
+
+pub fn set_request_url(s: &str) {
+	unsafe { raw_set_request_url(s.as_ptr(), s.len()) };
+}
+
+pub fn set_request_url_prefix(s: &str) {
+	unsafe { raw_set_request_url_prefix(s.as_ptr(), s.len()) };
+}
+
+pub fn is_request_pending() -> bool {
+	unsafe { raw_is_request_pending() != 0 }
+}
+
+pub fn ensure_pending_request(app: &Application) {
+	if !is_request_pending() {
+		if let Some(data_request) = app.data_requests.last() {
+			set_request_url(&data_request.name);
+		}
+	}
+}
+
+#[allow(dead_code)]
+pub static mut APPLICATION: Option<Application> = None;
+pub static mut RESPONSE_BYTES: Option<Vec<u8>> = None;
+pub static mut BLITS_PIXELS: Option<HashMap<NodePath, (Spot, Vec<u8>)>> = None;
+pub static mut BG_PIXELS: Option<Vec<u8>> = None;
+pub static mut FRAME_SPOT: Option<Spot> = None;
+pub static mut BG_DIRTY: bool = true;
+
+pub fn blit<'a>(spot: &'a Spot, path: Option<&'a NodePath>) -> Option<(&'a mut [u8], usize, bool)> {
+	if let Some(path) = path {
+		let depth = path.len();
+		let (position, size) = *spot;
+		let total_pixels = size.w * size.h * RGBA;
+		let (saved_spot, slice) = unsafe {
+			let blits = BLITS_PIXELS.as_mut().unwrap();
+			if let None = blits.get_mut(path) {
+				let pixels = vec![0; total_pixels];
+				let spot = (Point::zero(), Size::zero());
+				blits.insert(path.clone(), (spot, pixels));
+			}
+			let (spot, vec) = blits.get_mut(path).unwrap();
+			vec.resize(total_pixels, 0);
+			(spot, &mut *vec)
+		};
+		let mut name = String::new();
+		for i in path {
+			if name.len() > 0 {
+				name.push('-');
+			}
+			name += &format!("{}", i);
+		}
+		if *saved_spot != *spot {
+			*saved_spot = *spot;
+			update_blit(position, size, slice, depth, &name);
+		} else {
+			set_blit_dirty(&name);
+		}
+		Some((slice, 0, true))
+	} else {
+		unsafe { BG_DIRTY = true };
+		let f_spot = unsafe { FRAME_SPOT.unwrap() };
+		let background = unsafe { BG_PIXELS.as_mut().unwrap() };
+		let background = background.as_mut_slice();
+		let (slice, pitch) = sub_spot(background, 0, [&f_spot, spot])?;
+		Some((slice, pitch, true))
+	}
+}
+
+#[export_name = "alloc_response_bytes"]
+pub extern fn alloc_response_bytes(len: usize) -> *const u8 {
+	let mut vec = Vec::with_capacity(len);
+	unsafe { vec.set_len(len) };
+	let ptr = vec.as_ptr();
+	unsafe { RESPONSE_BYTES = Some(vec) };
+	ptr
+}
+
+#[export_name = "process_response"]
+pub extern fn process_response(app: &mut Application) {
+	let request = app.data_requests.pop().unwrap();
+	let node = app.get_node(&request.node).unwrap();
+	let mut node = node.lock().unwrap();
+	let data = unsafe {
+		RESPONSE_BYTES.as_ref().unwrap()
+	};
+	node.loaded(app, &request.node, &request.name, 0, data);
+}
+
+#[export_name = "drop_response_bytes"]
+pub extern fn drop_response_bytes() {
+	unsafe {
+		RESPONSE_BYTES = None;
+	}
+}
+
+#[export_name = "discard_request"]
+pub extern fn discard_request(app: &mut Application) {
+	app.data_requests.pop().unwrap();
+}
+
+#[export_name = "set_output_size"]
+pub extern fn set_output_size(app: &mut Application, w: usize, h: usize) -> *const u8 {
+	let spot = (Point::zero(), Size::new(w, h));
+	let bg_pixels = unsafe {
+		FRAME_SPOT = Some(spot);
+		let bg_pixels = BG_PIXELS.as_mut().unwrap();
+		bg_pixels.resize(w * h * RGBA, 0);
+		bg_pixels.as_ptr()
+	};
+	app.set_spot(spot);
+	bg_pixels
+}
+
+#[export_name = "frame"]
+pub extern fn frame(app: &mut Application) -> usize {
+	let mut bg_dirty = false;
+	swap(unsafe { &mut BG_DIRTY }, &mut bg_dirty);
+	app.render();
+	ensure_pending_request(app);
+	match bg_dirty {
+		true => 1,
+		false => 0,
+	}
+}
+
+pub fn wasm_init(assets: &str, mut app: Application) -> &'static Application {
+	app.set_styles(vec![
+		Style {
+			background: [50, 50, 50, 255],
+			foreground: [255; RGBA],
+			border:     [0; RGBA],
+		},
+		Style {
+			background: [100, 100, 100, 255],
+			foreground: [100, 200, 100, 255],
+			border:     [0; RGBA],
+		},
+		Style {
+			background: [50, 50, 250, 255],
+			foreground: [255; RGBA],
+			border:     [0; RGBA],
+		}
+	]);
+	unsafe {
+		BG_PIXELS = Some(Vec::new());
+		set_request_url_prefix(&String::from(assets));
+		BLITS_PIXELS = Some(HashMap::new());
+		APPLICATION = Some(app);
+		&APPLICATION.as_ref().unwrap()
+	}
+}
+
+#[macro_export]
+macro_rules! app {
+	($path: literal, $init: block) => {
+		#[export_name = "init"]
+		pub extern fn init() -> &'static Application {
+			std::panic::set_hook(Box::new(|panic_info| {
+				let dbg = format!("{}", panic_info);
+				log(&dbg);
+			}));
+			platform::wasm_init($path, $init)
+		}
+	}
+}
